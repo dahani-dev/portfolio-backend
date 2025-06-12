e79b53257a6dd3ef85d0ac68bf60dece615ad8cd
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-06-12 21:14:04 +0100

    add the validations of inputs

--- src/login/dto/login.dto.ts
+++ src/login/dto/login.dto.ts
@@ -1,4 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class LoginDto {
+  // add some validation of inputs
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @IsNotEmpty()
   password: string;
 }
--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString, IsNotEmpty, IsUrl, MaxLength } from 'class-validator';
+
 export class CreateProjectDto {
+  // add some validation of inputs
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(500)
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   image: string;
+
+  @IsString()
+  @IsNotEmpty()
   category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
   link: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
   github: string;
 }
